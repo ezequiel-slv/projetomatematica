b008c53113e7076a5b6be9c0d68c3aa124c4e37b
John Vitor <johnvitoralves39@gmail.com>
2023-06-14 01:30:15 -0300

    Update ColorGrafo.py

--- ColorGrafo.py
+++ ColorGrafo.py
@@ -1,55 +1,55 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def vertex_coloring(graph):
+def vertice_coloring(grafo):
     # Passo 1: Ordenar os vértices de acordo com graus decrescentes
-    sorted_vertices = sorted(graph.nodes(), key=lambda x: graph.degree[x], reverse=True)
+    ordenar_vertices = sorted(grafo.nodes(), key=lambda x: grafo.degree[x], reverse=True)
 
     # Dicionário para armazenar as cores dos vértices
-    colors = {}
+    cores = {}
 
     # Passo 2 a 4: Atribuir cores aos vértices
-    for vertex in sorted_vertices:
+    for vertex in ordenar_vertices:
         # Conjunto de cores disponíveis para o vértice atual
-        available_colors = set(range(1, len(graph) + 1))
+        cores_disponiveis = set(range(1, len(grafo) + 1))
 
         # Verificar cores dos vértices adjacentes
-        for neighbor in graph.neighbors(vertex):
-            if neighbor in colors:
-                available_colors.discard(colors[neighbor])
+        for neighbor in grafo.neighbors(vertex):
+            if neighbor in cores:
+                cores_disponiveis.discard(cores[neighbor])
 
         # Atribuir a menor cor disponível ao vértice
-        colors[vertex] = min(available_colors)
+        cores[vertex] = min(cores_disponiveis)
 
-    return colors
+    return cores
 
 
 # Função para visualizar o grafo colorido
-def visualize_colored_graph(graph, colors):
-    pos = nx.circular_layout(graph)
+def visualisar_grafo_colorido(grafo, cores):
+    pos = nx.shell_layout(grafo)
 
-    node_colors = [colors[node] for node in graph.nodes()]
+    node_colors = [cores[node] for node in grafo.nodes()]
 
-    nx.draw(graph, pos, with_labels=True, node_color=node_colors, cmap=plt.cm.Set1, node_size=500)
+    nx.draw(grafo, pos, font_color='white', with_labels=True, node_color=node_colors, cmap=plt.cm.Set1, node_size=500)
     plt.show()
 
 
 # Entrada do usuário: número de vértices e arestas do grafo
 num_vertices = int(input("Digite o número de vértices do grafo: "))
-num_edges = int(input("Digite o número de arestas do grafo: "))
+num_arestas = int(input("Digite o número de arestas do grafo: "))
 
 # Criação do grafo
-graph = nx.Graph()
-graph.add_nodes_from(range(1, num_vertices + 1))
+grafo = nx.Graph()
+grafo.add_nodes_from(range(1, num_vertices + 1))
 
 # Adição das arestas
 print("Digite as arestas no formato 'vértice1 vértice2':")
-for _ in range(num_edges):
-    edge = input().split()
-    graph.add_edge(int(edge[0]), int(edge[1]))
+for _ in range(num_arestas):
+    arestas = input().split()
+    grafo.add_edge(int(arestas[0]), int(arestas[1]))
 
 # Execução do algoritmo de coloração
-colors = vertex_coloring(graph)
+cores = vertice_coloring(grafo)
 
 # Visualização do grafo colorido
-visualize_colored_graph(graph, colors)
+visualisar_grafo_colorido(grafo, cores)
